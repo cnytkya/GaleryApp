5baf347f986438f653e1c18f2bf3317e802ae689
yurdsevenbusem708@gmail.com <yurdsevenbusem708@gmail.com>
2025-05-03 13:16:30 +0300

    #12

---
+++ GaleryApp.Data/IGenericRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GaleryApp.Data
+{
+    public interface IGenericRepository<T> where T : class
+
+    {
+        void Create(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+        List<T> GetAll();
+        T GetById(int id);
+    }
+
+}
+
