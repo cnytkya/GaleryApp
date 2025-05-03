05a24a11b9c0f613b64a18eac88e4fc678f7a4d0
Feyza Hacıalioğlu <feyzahacialiogluu@gmail.com>
2025-05-03 13:20:21 +0300

    #10  i created baseEntity and IEntity

--- GaleryApp.Models/Entities/kamyonet.cs
+++ GaleryApp.Models/Entities/kamyonet.cs
@@ -1,17 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using GaleryApp.Models.Abstract;
 
 namespace GaleryApp.Models.Entities
 {
-    public class kamyonet
+    public class kamyonet : BaseEntity
     {
-        public int Id { get; set; }
-        public string Marka { get; set; }
-        public string Model { get; set; }
-        public int Yil { get; set; }
+
         public int YukKapasitesi { get; set; }
     }
 }
